2f53ae68f61ea9cf0958c3c826b0af9c0f983b6f
p-wonnert <philipp_wonnerth@hotmail.de>
2020-12-01 15:05:52 +0100

    Update sortieren.js

--- Code/routes/sortieren.js
+++ Code/routes/sortieren.js
@@ -11,12 +11,31 @@ router.get('/:barcode', (req, res) => {
                 const res = await fetch(`https://world.openfoodfacts.org/api/v0/product/${barcode}.json`)
                 const fetchData = await res.json();
 
-                console.log(fetchData.product.labels)
+                const siegel = new Array ('de:Eier aus Bodenhaltung','MSC','ASC');
 
-                if(fetchData.product.labels.match('MSC')){
-                    console.log('Hinzugefügt')
-                } else {
-                    console.log('Nicht nachhaltig')
+                /* Barcodes:
+                ASC= 4006451000367 Lachs
+                MSC=4250073450639 Thunfisch 
+                Nicht hachnaltig= 8000500310427 Nutella
+                */
+
+                console.log(fetchData.product.labels)
+                console.log(fetchData.product.product_name)
+
+                for (i=0;i<siegel.length;i++) {
+                    if (fetchData.product.labels.match(siegel[i])) {
+                        console.log(fetchData.product.product_name + ' der Marke ' + fetchData.product.brands + ' wurde hinzugefügt')
+                        break;
+                    } if (i==siegel.length && fetchData.product.labels != siegel[i]) {
+                        console.log(fetchData.product.product_name + ' der Marke ' + fetchData.product.brands + ' ist nicht nachhaltig')
+                    } if (fetchData.product.labels == '') {
+                        console.log(fetchData.product.product_name + ' der Marke ' + fetchData.product.brands + ' ist nicht nachhaltig')
+                        break;
+                    }
+
+
+
+                   
                 }
 
             } catch (error) {
