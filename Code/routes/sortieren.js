28583eb64b3750965abefa4a977a4e8c1ad34355
Ubeyde <ubeydedemirel@hotmail.de>
2020-12-01 13:51:52 +0100

    Update sortieren.js

--- Code/routes/sortieren.js
+++ Code/routes/sortieren.js
@@ -7,23 +7,37 @@ router.get('/:barcode', (req, res) => {
     const barcode = req.params.barcode;
     console.log('Request');
     const getData = async (barcode) => {
-            try {
-                const res = await fetch(`https://world.openfoodfacts.org/api/v0/product/${barcode}.json`)
-                const fetchData = await res.json();
+        try {
+            const res = await fetch(`https://world.openfoodfacts.org/api/v0/product/${barcode}.json`)
+            const fetchData = await res.json();
 
-                console.log(fetchData.product.labels)
+            const siegel = new Array('de:Eier aus Bodenhaltung', 'MSC');
 
-                if(fetchData.product.labels.match('MSC')){
-                    console.log('Hinzugefügt')
+            console.log(fetchData.product.labels)
+            console.log(fetchData.product.product_name)
+            console.log(fetchData.product.product_name)
+
+            for (i = 0; i < siegel.length;) {
+                if (fetchData.product.labels != siegel[i]) {
+                    i++;
+                } if (fetchData.product.labels.match(siegel[i])) {
+                    console.log(fetchData.product.product_name + ' der Marke ' + fetchData.product.brands + ' wurde hinzugefügt')
+                    break;
                 } else {
-                    console.log('Nicht nachhaltig')
+                    console.log(fetchData.product.product_name + ' der Marke ' + fetchData.product.brands + ' ist nicht nachhaltig')
                 }
-
-            } catch (error) {
-                console.log(error);
             }
-        } 
-    getData(barcode);  
+            /*if(fetchData.product.labels.match(siegel)){
+                console.log(fetchData.product.generic_name_de + ' wurden hinzugefügt')
+            } else {
+                console.log(fetchData.product.generic_name_de + ' ist nicht nachhaltig')
+            }*/
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    getData(barcode);
 })
 
 module.exports = router
