9058642254b8c8c79de383707139bff1bbdf39bf
p-wonnerth <philipp_wonnerth@hotmail.de>
2021-01-25 00:43:20 +0100

    Externe Datenbank Cloud implementiert
    Mongodb Atlas Cloud

--- Rapid Prototype/routes/kassenbon.js
+++ Rapid Prototype/routes/kassenbon.js
@@ -19,26 +19,27 @@ router.get('/', async (req, res) => {
 
         let promises = [];
         let produkte = [];
-    
+
         for (var u = 0; u <= req.body.barcode.length; u++) {
 
             const getData = async () => {
                 try {
                     const res = await fetch(`https://de.openfoodfacts.org/api/v0/product/` + req.body.barcode[u] + `.json`)
                     const fetchData = await res.json()
-    
+
                     const siegel = new Array('Demeter', 'Bioland', 'Naturland')
-                    
-                    
-    
-                    
-                    
+
+
+
+
+
                     if (fetchData.status == 1) {
                         if (fetchData.product.labels.match('Hergestellt in Deutschland')) {
-                            if (checkBioSiegel != 1) {                            
+                            if (checkBioSiegel != 1) {
                                 var code =  await checkBioSiegel(siegel, fetchData);
-                                produkte.push(code);
-                                //console.log(produkte)                       
+                                if (code != 1) {
+                                    produkte.push(code)
+                                }
                             }
                             if (checkBioSiegel == 1) {
                                 console.log('Produkte wurden nicht gepostet')
@@ -47,22 +48,22 @@ router.get('/', async (req, res) => {
                         if ( !fetchData.product.labels.match('Hergestellt in Deutschland')) {
                             console.log(fetchData.product.product_name_de + " der Marke " + fetchData.product.brands + " kommt nicht aus Deutschland.")
                         }
-                    } 
+                    }
                     if (fetchData.status == 0) {
                         console.log("Produkt wurde nicht gefunden!")
-                    }  
-                                
+                    }
+
                 }
                 catch (error) {
                     console.log(error);
                 }
             }
-            
+
             promises.push(getData())
 
-    
+
         }
-        
+
         return Promise.all(promises).then( () => produkte)
     }
 
@@ -72,7 +73,7 @@ router.get('/', async (req, res) => {
             userId: req.body.userId,
             produkte: produkte
             })
-             try {      
+             try {
                 const newKassenbon = await kassenbon.save()
                 res.status(201).json(newKassenbon)
             } catch (err) {
@@ -80,8 +81,8 @@ router.get('/', async (req, res) => {
             }
     })
 
-    
-}) 
+
+})
 
 
 function checkBioSiegel(array, json) {
@@ -95,9 +96,9 @@ function checkBioSiegel(array, json) {
         }
         if (i==array.length && json.product.labels != array[i]) {
             console.log(json.product.product_name_de + ' der Marke ' + json.product.brands + ' kommt aus Deutschland, ist aber nicht nachhaltig.')
-            return 1
+            return 1;
         }
-    } 
+    }
 }
 
 router.delete('/:id', getKassenbon, async (req, res) => {
@@ -119,7 +120,7 @@ async function getKassenbon(req, res, next) {
     } catch(err) {
         return res.status(500).json({ message: err.message})
     }
-    
+
     res.kassenbon = kassenbon
     next()
 }
--- Rapid Prototype/server.js
+++ Rapid Prototype/server.js
@@ -4,10 +4,25 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+/* mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Mit Datenbank verbunden'))
+db.once('open', () => console.log('Mit Datenbank verbunden'))*/
+
+const url = `mongodb+srv://WonnerthDemirel:demirelwonnerth@produkte.mxh9y.mongodb.net/products?retryWrites=true&w=majority`;
+
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Mit Datenbank verbunden')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
 
 app.use(express.json())
 
