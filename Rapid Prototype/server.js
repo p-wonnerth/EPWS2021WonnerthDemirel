3ba000983571230dbd7c558ce6f351cb238e739f
p-wonnerth <philipp_wonnerth@hotmail.de>
2021-02-15 11:56:52 +0100

    server aktualisiert

--- Rapid Prototype/server.js
+++ Rapid Prototype/server.js
@@ -4,11 +4,6 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-/* mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Mit Datenbank verbunden'))*/
-
 const url = `mongodb+srv://WonnerthDemirel:demirelwonnerth@produkte.mxh9y.mongodb.net/products?retryWrites=true&w=majority`;
 
 const connectionParams={
@@ -26,16 +21,6 @@ mongoose.connect(url,connectionParams)
 
 app.use(express.json())
 
-const sortierenRouter = require('./routes/sortieren')
-app.use('/sortieren', sortierenRouter)
-
-const testRouter = require('./routes/test')
-app.use('/test', testRouter)
-
-//const kassenbonRouter = require('./routes/kassenbon')
-//app.use('/kassenbon', kassenbonRouter)
-
-//test
 const userRouter = require('./routes/user')
 app.use('/user', userRouter)
 
